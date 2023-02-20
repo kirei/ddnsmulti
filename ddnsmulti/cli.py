bf99c3ff57d71eb0186452286ef97416500b3f4e
Jakob Schlyter <jakob@kirei.se>
2023-02-20 17:29:01 +0100

    implement sending updates and nsupdate

--- ddnsmulti/cli.py
+++ ddnsmulti/cli.py
@@ -1,6 +1,9 @@
 import argparse
 import logging
 
+import dns.query
+import dns.tsig
+
 from .change_request import ChangeRequest  # noqa
 from .config import UpdaterConfig
 from .queue import ChangeRequestQueue
@@ -10,7 +13,7 @@ logger = logging.getLogger(__name__)
 DEFAULT_CONFIG_FILE = "ddnsmulti.yaml"
 
 
-def show_queue(config: dict, args: argparse.Namespace):
+def show_queue(config: UpdaterConfig, args: argparse.Namespace):
     queue = ChangeRequestQueue(
         queue_directory=config.queue_directory, index=config.index
     )
@@ -19,7 +22,7 @@ def show_queue(config: dict, args: argparse.Namespace):
         print(f"- {qe.filename} ({qe.fingerprint})")
 
 
-def update_queue(config: dict, args: argparse.Namespace):
+def update_queue(config: UpdaterConfig, args: argparse.Namespace):
     queue = ChangeRequestQueue(
         queue_directory=config.queue_directory, index=config.index
     )
@@ -28,8 +31,72 @@ def update_queue(config: dict, args: argparse.Namespace):
     queue.save_index()
 
 
-def send_dns_updates(config: dict, args: argparse.Namespace):
-    pass
+def send_dns_updates(config: UpdaterConfig, args: argparse.Namespace):
+    queue = ChangeRequestQueue(
+        queue_directory=config.queue_directory, index=config.index
+    )
+
+    if config.index:
+        logging.info("Load index")
+        queue.load_index()
+    else:
+        logging.info("Using raw queue")
+        queue.update_queue()
+
+    if args.nsupdate:
+        for qe in queue:
+            nsupdate = qe.cr.to_nsupdate()
+            print(f"; {qe.filename}")
+            print(nsupdate)
+            print("send")
+            print()
+    else:
+        for qe in queue:
+            for nameserver in config.nameservers:
+                address = str(nameserver["address"])
+                if address not in qe.nameservers:
+                    print("Will update", nameserver["address"])
+                    update = qe.cr.to_message()
+
+                    if tsig := nameserver.get("tsig"):
+                        key = dns.tsig.Key(
+                            name=tsig["name"], secret=tsig["key"], algorithm=tsig["alg"]
+                        )
+                        update.use_tsig(keyring=key)
+                    try:
+                        response = dns.query.tcp(
+                            update, address, port=nameserver["port"], timeout=10
+                        )
+                        if response.rcode():
+                            qe.set_nameserver_incomplete(address)
+                            logging.warning(
+                                "%s (%s) not accepted by %s",
+                                qe.filename,
+                                qe.fingerprint,
+                                address,
+                            )
+                        else:
+                            qe.set_nameserver_complete(address)
+                            logging.info(
+                                "%s (%s) accepted by %s",
+                                qe.filename,
+                                qe.fingerprint,
+                                address,
+                            )
+                    except ConnectionRefusedError:
+                        qe.set_nameserver_incomplete(address)
+                        logging.warning(
+                            "%s (%s) connection refused by %s",
+                            qe.filename,
+                            qe.fingerprint,
+                            address,
+                        )
+                else:
+                    print(nameserver["address"], "skipped, already sent")
+
+    if config.index:
+        logging.info("Save index")
+        queue.save_index()
 
 
 def main() -> None:
@@ -41,6 +108,9 @@ def main() -> None:
         metavar="config",
         default=DEFAULT_CONFIG_FILE,
         help="Configuration file",
+    )
+    parser.add_argument(
+        "--nsupdate", action="store_true", help="Output nsupdate commands"
     )
     parser.add_argument("--debug", action="store_true", help="Enable debugging")
 
